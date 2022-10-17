192cc8ecec8c48e09695f60fa527dc49c5955898
1vijit <111475826+1vijit@users.noreply.github.com>
2022-10-17 11:24:12 -0400

    final push

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pandas
 import csv
-# user = input("What is your name?": )
-user = "Vijit"
+user = input("What is your name? :").upper()
 user_list = [alpha for alpha in user]
-print(user_list)
+# print(user_list)
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-# data_dict = {letter:code for data.letter}
-with open('nato_phonetic_alphabet.csv', mode='r') as inp:
-    reader = csv.reader(inp)
-    dict_from_csv = {rows[0]:rows[1] for rows in reader}
 
-output = []
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows() }
+print(phonetic_dict)
+
+for letter in user:
+    phone = phonetic_dict[letter]
+    print(f"{letter} : {phone}")
